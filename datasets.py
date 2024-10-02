77171fdaed717b6f5519f7238bb29dee811bd624
breadbread1984 <breadbread1984@163.com>
2024-10-02 16:57:13 +0800

    updatge

--- datasets.py
+++ datasets.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 
 import pandas as pd
+from multiprocessing.pool import ThreadPool
 import gymnasium as gym
 import torch
 from torch.utils.data import Dataset
 
+def get_shape(atari_env):
+  env = gym.make(atari_env, render_mode = 'human')
+  observation, info = env.reset()
+  action = env.action_space.sample()
+  observation, reward, done, _, info = env.step(action)
+  env.close()
+  return observation.shape
+
 def collect_episodes(model, num = 100, out_path = 'episodes.csv'):
-  atari_envs = [env_id for env_id in gym.envs.registry.keys() if 'ALE/' in env_id]
-  shapes = list()
-  for atari_env in atari_envs:
-    env = gym.make(atari_env, render_mode = 'human')
-    observation, info = env.reset()
-    done = False
-    shapes.append(observation.shape)
+  atari_envs = [env_id for env_id in gym.envs.registry.keys() if 'ALE/' in env_id and 'ram' not in env_id]
+  print(atari_envs)
+  pool = ThreadPool(processes = 10)
+  results = pool.map_async(get_shape, atari_envs)
+  shapes = results.get()
   import pickle
   with open('shapes.pkl', 'wb') as f:
     f.write(pickle.dumps(shapes))
