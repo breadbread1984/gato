8dad2453baf717b59c9f5a64b88ec19514c76ef4
breadbread1984 <breadbread1984@163.com>
2024-10-02 10:03:57 +0800

    update

---
+++ datasets.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import pandas as pd
+import gymnasium as gym
+import torch
+from torch.utils.data import Dataset
+
+def collect_episodes(model, num = 100, out_path = 'episodes.csv'):
+  atari_envs = [env_id for env_id in gym.envs.registry.keys() if 'ALE/' in env_id]
+  shapes = list()
+  for atari_env in atari_envs:
+    env = gym.make(atari_env, render_mode = 'human')
+    observation, info = env.reset()
+    done = False
+    shapes.append(observation.shape)
+  import pickle
+  with open('shapes.pkl', 'rb') as f:
+    f.write(pickle.dumps(shapes))
+
+class TrajectoryDataset(Dataset):
+  def __init__(self):
+    super(TrajectoryDataset, self):
+
+if __name__ == "__main__":
+  collect_episodes('test')
