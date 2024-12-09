98c4e390e60ada58fe86c8cb9a7421449f2405a9
breadbread1984 <breadbread1984@163.com>
2024-12-09 11:25:44 +0800

    update

--- datasets.py
+++ datasets.py
@@ -1,31 +1,75 @@
 #!/usr/bin/python3
 
-import pandas as pd
-from multiprocessing import Pool, cpu_count
 import gymnasium as gym
+import ale_py
+import numpy as np
+import cv2
 import torch
 from torch.utils.data import Dataset
 
-def get_shape(atari_env):
-  env = gym.make(atari_env, render_mode = 'human')
-  observation, info = env.reset()
-  action = env.action_space.sample()
-  observation, reward, done, _, info = env.step(action)
-  env.close()
-  return observation.shape
+def preprocess(obs):
+  obs = cv2.resize(obs, (224,224)) # obs.shape = (224,224,3)
+  obs = np.reshape(obs, (32,7,32,7,3)) # obs.shape = (32,7,32,7,3)
+  obs = np.transpose(obs, (1,3,0,2,4)) # obs.shape = (7,7,32,32,3)
+  obs = np.reshape(obs, (49, 1024 * 3)) # obs.shape = (49, 1024 * 3)
+  return obs
 
-def collect_episodes(model, num = 100, out_path = 'episodes.csv'):
+def discount_cumsum(rewards, gamma = 1.):
+  discount_cumsum = np.zeros_like(rewards) # discount_cumsum.shape = (len)
+  discount_cumsum[-1] = rewards[-1]
+  for t in reversed(range(rewards.shape[0] - 1)):
+    discount_cumsum[t] = x[t] + gamma * discount_cumsum[t + 1]
+  return discount_cumsum
+
+def load_trajectories(trajectories = 10, policy = None, seed = None):
+  gym.register_envs(ale_py)
+  # pick a Atari env
   atari_envs = [env_id for env_id in gym.envs.registry.keys() if 'ALE/' in env_id and 'ram' not in env_id]
-  print(atari_envs)
-  with Pool(processes = cpu_count()) as pool:
-    results = pool.map(get_shape, atari_envs)
-  import pickle
-  with open('shapes.pkl', 'wb') as f:
-    f.write(pickle.dumps(results))
+  choice = np.random.randint(low = 0, high = len(atari_envs), size = ())
+  env = gym.make(atari_envs[choice], render_mode = "rgb_array")
+  # collect trajectories
+  trajectories = list()
+  for _ in range(trajectories):
+    states, rewards, actions, dones, returns = list(), list(), list(), list(), list()
+    obs, info = env.reset(seed = seed)
+    states.append(preprocess(obs)) # s_t
+    while True:
+      if policy is None:
+        action = env.action_space.sample()
+      else:
+        # TODO: use policy
+        pass
+      obs, reward, done, trunc, info = env.step(action)
+      rewards.append(reward) # r_t
+      actions.append(action) # a_t
+      dones.append(done)
+      returns.append(sum(rewards)) # V(s)
+      if done:
+        assert len(states) == len(actions) == len(rewards) == len(dones)
+        trajectories.append({'observations': np.stack(states, axis = 0), # shape = (len, 49, 1024 * 3)
+                             'actions': np.stack(actions, axis = 0), # shape = (len)
+                             'rewards': np.stack(rewards, axis = 0), # shape = (len)
+                             'dones': np.stack(dones, axis = 0), # shape = (len)
+                             'returns': np.stack(returns, axis = 0)}) # shape = (len)
+        trajectories[-1]['rtg'] = discount_cumsum(trajectories[-1]['rewards'])
+        break
+      states.append(preprocess(obs)) # s_{t+1}
+  
+  return trajectories
 
 class TrajectoryDataset(Dataset):
   def __init__(self):
     super(TrajectoryDataset, self).__init__()
 
 if __name__ == "__main__":
-  collect_episodes('test')
+  import cv2;
+  env = load_env()
+  observation, info = env.reset(seed = 42)
+  for _ in range(100):
+    action = env.action_space.sample()
+    obs, reward, done, trunc, info = env.step(action)
+    #print(obs.shape)
+    image = env.render()[:,:,::-1]
+    cv2.imshow("", image)
+    cv2.waitKey(20)
+
