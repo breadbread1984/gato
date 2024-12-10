4fefc2abeaa13f519ff7581fadc68cc8bfaf1568
breadbread1984 <breadbread1984@163.com>
2024-12-10 08:58:46 +0800

    update

--- models.py
+++ models.py
@@ -60,19 +60,21 @@ class Gato(nn.Module):
     self.pi = nn.Linear(self.llama3.config.hidden_size, 18)
     self.v_value = nn.Linear(self.llama3.config.hidden_size, 1)
     self.patch_size = patch_size
-  def forward(self, inputs):
+  def forward(self, inputs, past_key_values = None):
     # inputs.shape = (batch, 3, 224, 224)
-    # hist.shape = (batch, hist_len, hidden)
+    # past_key_values.shape = (layer_num, 2, batch, head, seq_len, hidden / head)
     results = (inputs - 128.) / 128. / torch.sqrt(self.patch_size)
     results = self.conv2d(inputs) # results.shape = (batch, hidden, 7, 7)
     results = torch.flatten(results, start_dim = 2) # results.shape = (batch, hidden, 49)
     results = torch.permute(results, (0,2,1)) # results.shape = (batch, 49, hidden)
-    attention_mask = torch.ones((results.shape[0], results.shape[1]), torch.int64) # attention_mask.shape = (batch, 49)
-    outputs = self.llama3.forward(inputs_embeds = results, attention_mask = attention_mask, use_cache = True)
+    seq_length = past_key_values[0][0].shape[2]
+    attention_mask = torch.ones((results.shape[0], seq_length + results.shape[1]), torch.int64) # attention_mask.shape = (batch, 49)
+    outputs = self.llama3.forward(inputs_embeds = results, attention_mask = attention_mask, past_key_values = past_key_values, use_cache = True)
     logits = outputs.last_hidden_state[:,-1,:] # logits.shape = (batch, hidden)
+    past_key_values = outputs.past_key_values
     action = torch.softmax(self.pi(logits), dim = -1) # action.shape = (batch, 18)
     v_value = self.v_value(logits) # v.shape = (batch, 1)
-    return action, v_value
+    return action, v_value, past_key_values
 
 if __name__ == "__main__":
   gato = Gato().to('cuda')
